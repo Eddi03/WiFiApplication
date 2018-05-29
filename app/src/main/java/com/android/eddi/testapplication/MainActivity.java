2a1d32cda62750b5f08dfed9f3eb026e9d10486a
Eddi Raimondi <raimondi.eddy2010@libero.it>
2018-05-29 17:14:44 +0200

    Added wifi network information

--- app/src/main/java/com/android/eddi/testapplication/MainActivity.java
+++ app/src/main/java/com/android/eddi/testapplication/MainActivity.java
@@ -1,22 +1,31 @@
 package com.android.eddi.testapplication;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
+import android.widget.NumberPicker;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Formatter;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView textViewBlue, textViewWiFi;
+    TextView textViewWiFi, textViewInfo;
     Switch aSwitchWiFi;
     WifiManager wifiManager;
+    WifiInfo wifiInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +34,12 @@ public class MainActivity extends AppCompatActivity {
 
         textViewWiFi = findViewById(R.id.textViewWiFi);
         aSwitchWiFi = findViewById(R.id.switchWiFi);
+        textViewInfo = findViewById(R.id.textViewInfo);
+
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
-        if (wifiManager.isWifiEnabled() == true){
+        if (wifiManager.isWifiEnabled()) {
+            writeWiFiInformation();
             aSwitchWiFi.setChecked(true);
             textViewWiFi.setText("Wi-Fi ON");
         }
@@ -35,14 +47,43 @@ public class MainActivity extends AppCompatActivity {
         aSwitchWiFi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
+                if (isChecked) {
                     textViewWiFi.setText("Wi-Fi ON");
+                    textViewInfo.setText("Loading Wi-Fi Information");
                     wifiManager.setWifiEnabled(true);
-                }else {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            writeWiFiInformation();
+                        }
+                    }, 6000);
+                } else {
                     textViewWiFi.setText("Wi-Fi OFF");
+                    textViewInfo.setText("No Wi-Fi Information");
                     wifiManager.setWifiEnabled(false);
                 }
             }
         });
+    }
+
+    public void writeWiFiInformation() {
+        wifiInfo = wifiManager.getConnectionInfo();
+        int ip = wifiInfo.getIpAddress();
+        @SuppressLint("HardwareIds") String macAddress = wifiInfo.getMacAddress();
+        String bssid = wifiInfo.getBSSID();
+        int rssi = wifiInfo.getRssi();
+        int linkSpeed = wifiInfo.getLinkSpeed();
+        String ssid = wifiInfo.getSSID();
+        int networkId = wifiInfo.getNetworkId();
+        String ipAddress = android.text.format.Formatter.formatIpAddress(ip);
+        String info = "Wi-Fi Information: " +
+                "\n" + "IpAddress: " + ipAddress +
+                "\n" + "MAC Address: " + macAddress +
+                "\n" + "BSSID: " + bssid +
+                "\n" + "RSSI: " + rssi +
+                "\n" + "Link Speed: " + linkSpeed +
+                "\n" + "SSID: " + ssid +
+                "\n" + "Network ID: " + networkId;
+        textViewInfo.setText(info);
     }
 }
